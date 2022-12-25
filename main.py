a9f61c5141bcd28a965e664a22d0019752bc376b
Wendi Fan <wendi.fan@softwire.com>
2022-12-25 22:04:57 +0000

    Starting Day 2

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 def parse_file():
-    given_input = open('input.txt', 'r')
+    given_input = open('input_2.txt', 'r')
     lines = given_input.readlines()
 
     formatted_list = [item.strip() for item in lines]
 
-    # print(f'Parsed file with {len(formatted_list)} lines')
     return formatted_list
 
 
@@ -16,39 +15,7 @@ def main():
 
     formatted_input = parse_file()
 
-    current_max = 0
-    running_count = 0
-    sums = []
-    for line in formatted_input:
-        if line == '':
-            sums.append(running_count)
-            running_count = 0
-        else:
-            running_count += int(line)
-    sums.sort(reverse=True)
-    print(sum(sums[:3]))
-    # while True:
-    #     line = given_input.readline()
-    #
-    #     if not line:
-    #         break
-    #
-    #     (current_max, running_count) = do_first_part(line, current_max, running_count)
-    #
-
-    # print(current_max)
-    return current_max
-
-
-def do_first_part(line, current_max, running_count):
-    if line == '\n':
-        if running_count > current_max:
-            current_max = running_count
-            print(current_max)
-        running_count = 0
-    else:
-        running_count += int(line)
-    return current_max, running_count
+    return "foo"
 
 
 if __name__ == '__main__':
