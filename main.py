b435c93522cdef06a018602181bb5337114a61cd
Wendi Fan <wendi.fan@softwire.com>
2022-12-25 22:03:09 +0000

    Part 2 done

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def parse_file():
+    given_input = open('input.txt', 'r')
+    lines = given_input.readlines()
+
+    formatted_list = [item.strip() for item in lines]
+
+    # print(f'Parsed file with {len(formatted_list)} lines')
+    return formatted_list
+
+
 def main():
     print('Running...')
-    given_input = open('input.txt', 'r')
-    count = 0
+
+    formatted_input = parse_file()
+
     current_max = 0
     running_count = 0
+    sums = []
+    for line in formatted_input:
+        if line == '':
+            sums.append(running_count)
+            running_count = 0
+        else:
+            running_count += int(line)
+    sums.sort(reverse=True)
+    print(sum(sums[:3]))
+    # while True:
+    #     line = given_input.readline()
+    #
+    #     if not line:
+    #         break
+    #
+    #     (current_max, running_count) = do_first_part(line, current_max, running_count)
+    #
 
-    while True:
-        count += 1
-        line = given_input.readline()
-
-        if not line:
-            break
-
-        (current_max, running_count) = do_first_part(line, current_max, running_count)
-
-
-    given_input.close()
-    print(count)
-    print(current_max)
+    # print(current_max)
     return current_max
 
 
