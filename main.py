1d3a9b70bde3906a043ca33daad35dd317fc8066
Wendi Fan <wendi.fan@softwire.com>
2022-12-25 21:18:15 +0000

    Basic file parser

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def print_hi():
+    print('Running...')
+    given_input = open('input.txt', 'r')
+    count = 0
 
-def print_hi(name):
-    print(f'Hi, {name}')
+    while True:
+        count += 1
+        line = given_input.readline()
+
+        if not line:
+            break
+
+    given_input.close()
+    print(count)
+
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
