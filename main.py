5c368ada922498100c7b27339478ce5c1622a9cd
Wendi Fan <wendi.fan@softwire.com>
2022-12-25 21:35:30 +0000

    Part 1 complete reading line by line

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi():
+def main():
     print('Running...')
     given_input = open('input.txt', 'r')
     count = 0
+    current_max = 0
+    running_count = 0
 
     while True:
         count += 1
@@ -13,10 +15,26 @@ def print_hi():
         if not line:
             break
 
+        (current_max, running_count) = do_first_part(line, current_max, running_count)
+
+
     given_input.close()
     print(count)
+    print(current_max)
+    return current_max
 
+
+def do_first_part(line, current_max, running_count):
+    if line == '\n':
+        if running_count > current_max:
+            current_max = running_count
+            print(current_max)
+        running_count = 0
+    else:
+        running_count += int(line)
+    return current_max, running_count
 
 
 if __name__ == '__main__':
-    print_hi()
+    answer = main()
+    print(answer)
